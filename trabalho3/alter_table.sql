90ce8d2dc059fb41b9f5e0f9702a7da78646a432
3uMesma <kasatamanda@gmail.com>
2026-04-18 16:51:30 -0300

    feat: criar a relação entre countries, drivers e constrcutros por chave extrangeira

--- trabalho3/alter_table.sql
+++ trabalho3/alter_table.sql
@@ -1,12 +1,10 @@
--- Ainda countries, mas agora add nacionalidades (gentílicos)
--- CASO RHODESIAN (é atualmente a região de Zimbabwe) --
--- CASO BRITISH:  England, Scotland, Wales, or Northern Ireland --
--- CASO HONG KONG (não é tecnicamente um país), nem Liechtenstein, nem Monaco
+-- INÍCIO DE MUDANÇA DE COUNTRIES
 
-ALTER TABLE countries ADD COLUMN IF NOT EXISTS nationality VARCHAR(255);
+-- ANTES DE TUDO, CRIANDO O MAPEAMENTO DE PAÍS-NACIONALIDAE PARA USAR NAS PRÓXIMAS QUERIES (TABELA TEMPORÁRIA)
+-- (APENAS FOI INSERIDA AS NACIONALDADES QUE JÁ EXISTIAM NO BANCO)
 
-WITH mapping(nat, country_name) AS (
-  VALUES 
+CREATE TEMP TABLE nat_mapping(nat TEXT, country_name TEXT) AS 
+    VALUES 
   	('American', 'United States'),
   	('Argentine', 'Argentina'),
   	('Australian', 'Australia'),
@@ -45,8 +43,53 @@ WITH mapping(nat, country_name) AS (
     ('Thai', 'Thailand'),
     ('Uruguayan', 'Uruguay'),
     ('Venezuelan', 'Venezuela')
-)
+
+-- CRIANDO A COLUNA NACIONALIDADE AOS COUNTRIES
+
+ALTER TABLE countries ADD COLUMN IF NOT EXISTS nationality VARCHAR(255);
+
+-- INSERINDO AS NACIONALIDADES CORRETAS PARA CADA PAÍS
+
 UPDATE countries c
 SET nationality = m.nat
-FROM mapping m
+FROM nat_mapping m
 WHERE c.name = m.country_name;
+
+-- MUDANÇA DE DRIVERS E CONSTRUCTORS: MANTER COLUNA DE NACIONALIDADE PARA MANTER HISTÓRICO
+ALTER TABLE drivers
+RENAME COLUMN nationality TO nationality_deprecated;
+
+ALTER TABLE constructors
+RENAME COLUMN nationality TO nationality_deprecated;
+
+-- CRIAR A RELAÇÃO COUNTRY_ID COM DRIVERS E CONSTRUCTORS
+ALTER TABLE drivers
+ADD COLUMN country_id INT REFERENCES countries(id);
+
+ALTER TABLE constructors
+ADD COLUMN country_id INT REFERENCES countries(id);
+
+-- INSERIR DADOS PARA CONECTAR ESSA RELÇÃO COM OS DRIVERS E CONSTRUCTORS EXISTENTES
+UPDATE drivers d
+SET country_id = c.id
+FROM nat_mapping m
+JOIN countries c ON c.name = m.country_name
+WHERE d.nationality_deprecated = m.nat;
+
+UPDATE constructors con
+SET country_id = c.id
+FROM nat_mapping m
+JOIN countries c ON c.name = m.country_name
+WHERE con.nationality_deprecated = m.nat;
+
+-- CASO ESPECÍFICO DE RHODESIAN
+UPDATE drivers
+SET country_id = 249
+WHERE id IN (105, 401);
+
+UPDATE constructors SET country_id = 249 WHERE id = 77;
+
+-- APAGANDO A TABELA TEMPORÁRIA DE MAPEAMENTO
+DROP TABLE nat_mapping;
+
+-- FINAL DE MUDANÇA DE COUNTRIES
