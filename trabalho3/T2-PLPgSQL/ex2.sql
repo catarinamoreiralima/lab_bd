deddcbc28dfaed634641ee79803b827c021dbde1
3uMesma <kasatamanda@gmail.com>
2026-05-05 14:41:05 -0300

    feat: exercicios 2 e 3 do T2-PLPgSQL

---
+++ trabalho3/T2-PLPgSQL/ex2.sql
@@ -0,0 +1,30 @@
+-- Função Pilotos_Nacionalidade
+CREATE OR REPLACE FUNCTION Pilotos_Nacionalidade(piloto_nacionalidade TEXT)
+RETURNS VOID AS $$
+DECLARE
+    v_piloto RECORD;
+	v_contador INTEGER := 1;
+BEGIN
+    FOR v_piloto IN 
+        SELECT given_name, family_name 
+        FROM drivers d
+		JOIN countries co ON d.country_id = co.id
+        WHERE co.nationality = piloto_nacionalidade
+    LOOP
+		RAISE NOTICE '% Nome: % %', v_contador, v_piloto.given_name, v_piloto.family_name;
+		v_contador := v_contador + 1;
+	END LOOP;
+
+	IF v_contador = 1 THEN
+        RAISE NOTICE 'Nenhum piloto encontrado para a nacionalidade: %', piloto_nacionalidade;
+    END IF;
+END;
+$$ LANGUAGE PLPgSQL;
+
+-- Testando com algumas nacionalidades
+SELECT Pilotos_Nacionalidade('Brazilian');
+
+SELECT Pilotos_Nacionalidade('Chilean');
+
+-- Testando nacionalidade que não existe
+SELECT Pilotos_Nacionalidade('USPian');
---
+++ trabalho3/T2-PLPgSQL/ex3.sql
@@ -0,0 +1,32 @@
+-- Função Cidade_Chamada
+CREATE OR REPLACE FUNCTION Cidade_Chamada(cidade_nome TEXT)
+RETURNS VOID AS $$
+DECLARE
+    v_cidade RECORD;
+	v_cidades_count INTEGER := 1;
+BEGIN
+	SELECT COUNT(*) INTO v_cidades_count
+    FROM cities c
+    WHERE c."name" = cidade_nome;
+
+	RAISE NOTICE 'Contagem: %', v_cidades_count;
+
+    IF v_cidades_count > 0 THEN
+        FOR v_cidade IN 
+            SELECT c."name", c.population, co."name" AS country_name
+            FROM cities c
+            JOIN countries co ON c.country_id = co.id
+            WHERE c."name" = cidade_nome
+        LOOP
+            RAISE NOTICE 'Nome: %, População: %, País: %', v_cidade."name", v_cidade.population, v_cidade.country_name;
+        END LOOP;
+    END IF;
+END;
+$$ LANGUAGE PLPgSQL;
+
+-- Teste com cidades que existem
+SELECT Cidade_Chamada('York');
+SELECT Cidade_Chamada('São Carlos');
+
+-- Teste com uma cidade que não existe
+SELECT Cidade_Chamada('USP');
